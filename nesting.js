f6805d155afdb9dc53a8145b47626f882376bbb9
Spencer Allen <spencerallen@Spencers-MacBook-Pro.local>
2018-10-17 16:14:01 -0600

    first commit js-2-afternoon nesting.js

--- nesting.js
+++ nesting.js
@@ -52,6 +52,16 @@ var employees = [
 
 //Code Here
 
+function employeeUpdater() {
+  for(var i = 0; i < employees.length; i ++){
+    if(employees[i].firstName === 'Theo') {
+      employees.splice(i,1);
+    }else if(employees[i].firstName === 'Lorie'){
+      employees[i].department = 'HR';
+    }
+  }
+  return employees;
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -69,7 +79,28 @@ var workplaceAccidents = [12, 56, 44, 3, 29, 56, 56, 3, 7, 12];
 */
 
 //Code Here
-
+var removeDuplicates = () => {
+  for(var i = 0; i < workplaceAccidents.length; i ++){
+    for(var j = i + 1; j < workplaceAccidents.length; j ++){
+      if(workplaceAccidents[i] === workplaceAccidents[j]){
+        workplaceAccidents.splice(j, 1);
+        j--;
+      }
+    }
+  }
+  return workplaceAccidents;
+}
+// function removeDuplicates(){
+//   for(var i = 0; i < workplaceAccidents.length; i ++){
+//     for(var j = i + 1; j < workplaceAccidents.length; j ++){
+//       if(workplaceAccidents[i] === workplaceAccidents[j]){
+//         workplaceAccidents.splice(j, 1);
+//         j--;
+//       }
+//     }
+//   }
+//   return workplaceAccidents;
+// }
 
 
 ////////// PROBLEM 3 //////////
